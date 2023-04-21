6ee1123f4061dd24b2bc690455aa4ce694073373
Natalia Manzhikova <manzhikova-natalia@yandex.ru>
2023-04-21 16:14:42 +0300

    1/1

--- controllers/cards.js
+++ controllers/cards.js
@@ -133,7 +133,7 @@ function putLike(req, res) {
   )
     .then((card) => {
       if (!card) {
-        res.status(404).send({ message: 'Передан несуществующий _id карточки.' });// не существ. id
+        res.status(404).send({ message: 'Передан несуществующий _id карточки.' });
         return;
       }
       return res.send({ data: card });
@@ -149,8 +149,9 @@ function putLike(req, res) {
 }
 
 function deleteLike(req, res) {
+  const { cardId } = req.params;
   Card.findByIdAndUpdate(
-    req.params.cardId,
+    cardId,
     {
       $pull: { likes: req.user._id },
     },
@@ -159,15 +160,19 @@ function deleteLike(req, res) {
     },
   )
     .then((card) => {
-      if (!card) {
-        res.status(400).send({ message: 'Передан невалидный id' });
+      if (card) {
+        res.status(200).send({ data: card });
+      } else {
+        res.status(404).send({ message: 'Передан невалидный id' });
+      }
+    })
+    .catch((error) => {
+      if (error.name === 'DocumentNotFoundError') {
+        res.status(404).send({ message: 'Карточка с указанным id не найдена'});
         return;
       }
-      return res.send({ data: card });
-    })
-    .catch((err) => {
-      if (err.name === 'CastError') {
-        res.status(400).send({ message: 'Переданы некорректные данные при снятии лайка'});
+      if (error.name === 'CastError') {
+        res.status(400).send({ message: 'Неверный формат id карточки' });
         return;
       }
         res.status(500).send({ message: 'Произошла ошибка' });
