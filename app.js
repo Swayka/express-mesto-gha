df7b4dbb6b0dae3bfc43d525aed965b0d4e972c5
Natalia Manzhikova <manzhikova-natalia@yandex.ru>
2023-05-03 14:36:17 +0300

    14/1

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const { errors } = require('celebrate');
-const rootRouter = require('./routes/index');
+const auth = require('./middlewares/auth');
+const { userRouter, cardRouter } = require('./routes');
 const { loginValidation, userValidation } = require('./utils/validate');
 const { login, createUser } = require('./controllers/users');
 const errorHandler = require('./middlewares/errorHandler');
@@ -17,14 +18,19 @@ app.use(express.urlencoded({ extended: true }));
 
 app.post('/signin', loginValidation, login);
 app.post('/signup', userValidation, createUser);
-app.use('/', rootRouter);
 
-app.use(errors());
-app.use(errorHandler);
+app.use(auth);
+
+app.use('/users', userRouter);
+app.use('/cards', cardRouter);
 
 app.use('*', (req, res, next) => {
   next(new NotFoundError('Страница не найдена'));
 });
+
+app.use(errors());
+app.use(errorHandler);
+
 
 app.listen(PORT, () => {
   console.log(`Server started on port ${PORT}`);
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,7 @@
-const router = require('express').Router();
 const userRouter = require('./users');
 const cardRouter = require('./cards');
-const auth = require('../middlewares/auth');
 
-router.use('/users', auth, userRouter);
-router.use('/cards', auth, cardRouter);
-
-module.exports = router;
+module.exports = {
+  userRouter,
+  cardRouter,
+};
